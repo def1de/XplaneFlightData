6bd4deaad628a955d1ae614ae290a7a4bfbca59e
Alex Burnet <alexburnet03@gmail.com>
2026-01-30 15:47:57 +0000

    print data when failed

--- actions_test/test_outputs.py
+++ actions_test/test_outputs.py
@@ -157,6 +157,8 @@ def test_calculator(filename, arguments, expected_output):
         print("❌ JSON mismatch:")
         for err in errors:
             print(f" - {err}")
+        print("Json:")
+        print(output_data)
         exit(1)
     else:
         print("✅ Output matches expected data")
